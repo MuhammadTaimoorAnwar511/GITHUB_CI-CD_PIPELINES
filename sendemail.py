cf8ed6a0e15f3567a1a2a1acfc436fce7742011e
Muhammad Taimoor Anwar <129211565+MuhammadTaimoorAnwar511@users.noreply.github.com>
2024-04-25 01:19:24 +0500

    Update sendemail.py

--- sendemail.py
+++ sendemail.py
@@ -1,37 +1,18 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+import smtplib, ssl
+import os
 
-def send_email(subject, body, to_email, from_email, password, smtp_server, smtp_port):
-    # Create a multipart message
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['To'] = to_email
-    msg['Subject'] = subject
+port = 465
+smtp_server = "smtp.gmail.com"
+USERNAME = os.environ.get('USER_EMAIL')
+PASSWORD = os.environ.get('USER_PASSWORD')
+message = """\
+Subject: GitHub Email Report
 
-    # Attach the email body
-    msg.attach(MIMEText(body, 'plain'))
+This is your daily email report.
+"""
 
-    try:
-        # Set up the SMTP server and send the email
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()  # Enable security
-        server.login(from_email, password)  # Log in to the SMTP server
-        text = msg.as_string()  # Convert the message to a string
-        server.sendmail(from_email, to_email, text)
-        server.quit()
-        print("Email sent successfully")
-    except Exception as e:
-        print(f"Failed to send email: {e}")
+context = ssl.create_default_context()
 
-# Email details - replace placeholders with your actual details
-subject = "GitHub Workflow Notification"
-body = "The GitHub Actions workflow has completed successfully."
-to_email = "i211232@nu.edu.pk"
-from_email = "github.com"
-password = "vlfw nyuf uhbs ywte"
-smtp_server = "smtp.example.com"
-smtp_port = 587
-
-# Send the email
-send_email(subject, body, to_email, from_email, password, smtp_server, smtp_port)
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(USERNAME, PASSWORD)
+    server.sendmail(USERNAME, USERNAME, message)
